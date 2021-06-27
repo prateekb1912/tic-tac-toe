173489a6f6e6445c38e3c125aa526c06a995d893
prateek-ml <prateekb1912@gmail.com>
2021-06-27 09:59:17 +0530

    Configure kotlin to work without findViewbyId

--- app/src/main/java/com/criclytica/tictactoe/MainActivity.kt
+++ app/src/main/java/com/criclytica/tictactoe/MainActivity.kt
@@ -2,10 +2,22 @@ package com.criclytica.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Initialize the 2D array of buttons displayed on screen
+    lateinit var board: Array<Array<Button>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        board = arrayOf(
+            arrayOf(btn1, btn2, btn3),
+            arrayOf(btn4, btn5, btn6),
+            arrayOf(btn7, btn8, btn9)
+        )
     }
 }
